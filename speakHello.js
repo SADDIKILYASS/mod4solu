7ef06f73b19c1210660959896259abfdfb261f3a
SADDIKILYASS <77972798+SADDIKILYASS@users.noreply.github.com>
2021-10-25 20:19:24 +0200

    Update speakHello.js

--- speakHello.js
+++ speakHello.js
@@ -1,12 +1,10 @@
-(function (window) {
-  var speakWord = "Hello";
-  var helloSpeaker = {}
-   helloSpeaker.speak=  function  (name)  {
-      console.log(speakWord + " " + name);
-    };
 
-  speak = function (name)  {
-    console.log(speakWord + " " + name);
-  };
-  window.helloSpeaker=helloSpeaker; 
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
 })(window);
